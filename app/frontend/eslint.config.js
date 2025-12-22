ae11aba656a28fe515b4a0c236a99ee926c569e0
Pawel <47537562+Electro5218@users.noreply.github.com>
2025-12-22 03:47:52 +0100

    Add ESLint support for React in configuration

--- app/frontend/eslint.config.js
+++ app/frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -9,6 +10,9 @@ export default defineConfig([
 
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
@@ -24,6 +28,8 @@ export default defineConfig([
       },
     },
     rules: {
+      'react/jsx-uses-vars': 'error',
+
       'no-unused-vars': [
         'error',
         {
@@ -31,6 +37,7 @@ export default defineConfig([
           argsIgnorePattern: '^_',
         },
       ],
+
       'react/react-in-jsx-scope': 'off',
       'react/jsx-uses-react': 'off',
     },
